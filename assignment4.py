06b1ce6a69027c1bbc88fa285f55816dcd6e2839
nestofsaws <zims@nestofsaws.com>
2015-01-27 20:41:02 -0500

    edits assignment 4

---
+++ assignment4.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+bowler = 'Kade'
+average = 180
+game1 = 83
+game2 = 50
+game3 = 66
+newavg = (game1 + game2 + game3) / 3
+age = 33
+kilos = 70
+def convert_weight (x):
+	convert = 2.2
+	pounds = x * convert
+	return pounds
+	
+status = 'Legend'
+bowling_status = 'Trash'
+
+print "Our bowler's name is", bowler
+print "Normally, his bowling average is", average, ", but tonight it was", newavg, "."
+print "His scores tonight were %d, %d, and %d." % (game1, game2, game3)
+print "(If you divide the total of those games by three, you will find the average)"
+print "Did I mention that %s is a %s?" % (bowler, status)
+print "He lost over %d kilograms!" % kilos
+print "thats about", convert_weight(70), "pounds!!"
+print "he may be a %s, but his bowling is still %s." % (status, bowling_status)
